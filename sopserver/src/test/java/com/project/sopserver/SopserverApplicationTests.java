0e027fca40ffa6165e975e7161f96392f276ae3f
warin001234567 <33062525+warin001234567@users.noreply.github.com>
2019-10-22 21:54:36 +0700

    server

---
+++ sopserver/src/test/java/com/project/sopserver/SopserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sopserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
