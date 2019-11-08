69c15e7fd7d94fdde335f8394a32d37dfffc1687
warinsukanthmet <60070089@kmitl.ac.th>
2019-11-08 16:21:03 +0700

    authen

---
+++ authenservice/src/main/java/com/project/authenservice/model/RoleModel.java
@@ -0,0 +1,43 @@
+package com.project.authenservice.model;
+
+
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class RoleModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 60)
+    private RoleName name;
+
+    public RoleModel() {
+
+    }
+
+    public RoleModel(RoleName name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getName() {
+        return name;
+    }
+
+    public void setName(RoleName name) {
+        this.name = name;
+    }
+}
