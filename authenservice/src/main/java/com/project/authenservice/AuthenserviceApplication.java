63ca5a014a7173ad9f0d7b8baf55f181d6215cb9
warinsukanthmet <60070089@kmitl.ac.th>
2019-11-08 16:21:15 +0700

    authen

---
+++ authenservice/src/main/java/com/project/authenservice/AuthenserviceApplication.java
@@ -0,0 +1,31 @@
+package com.project.authenservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = {
+		AuthenserviceApplication.class,
+		Jsr310JpaConverters.class
+})
+public class AuthenserviceApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenserviceApplication.class, args);
+	}
+
+
+
+}
---
+++ authenservice/src/test/java/com/project/authenservice/AuthenserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.authenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
