cd09895692c243fec43e8ae49d0ab5eb29a28a99
ingchoff <ingkaratt@gmail.com>
2019-11-27 23:46:21 +0700

    fixed stock entity

--- stock/src/main/java/com/project/stock/entities/Stock.java
+++ stock/src/main/java/com/project/stock/entities/Stock.java
@@ -2,10 +2,7 @@ package com.project.stock.entities;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -13,6 +10,7 @@ import javax.validation.constraints.Size;
 
 @Data
 @Entity
+@Table(name = "stocks")
 public class Stock {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
